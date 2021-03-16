78760cc2980a3186aa8796e73f2f81e7ac33ee17
Daniel Subiabre <subiabrewd@gmail.com>
2021-03-16 23:03:08 +0100

    Update command options

--- src/Command/GenerateGifCommand.php
+++ src/Command/GenerateGifCommand.php
@@ -9,6 +9,7 @@ use GifCreator\GifCreator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class GenerateGifCommand extends Command
@@ -22,8 +23,10 @@ class GenerateGifCommand extends Command
         $this->addArgument('start', InputArgument::REQUIRED, 'Start date to look for pictures. YYYY-MM-DD');
         $this->addArgument('end', InputArgument::REQUIRED, 'End date to look for pictures. YYYY-MM-DD');
 
-        $this->addOption('margin', null, InputArgument::OPTIONAL, 'Amount of timezone deviation margin', 5);
-        $this->addOption('imagetype', null, InputArgument::OPTIONAL, 'Type of images to use as gif frames', 'jpg');
+        $this->addOption('timezone-margin', null, InputOption::VALUE_OPTIONAL, 'Amount of allowed timezone deviation margin', 5);
+        $this->addOption('sourcetype', null, InputOption::VALUE_OPTIONAL, 'Type of images to use as gif frames', 'jpg');
+        $this->addOption('frame-duration', null, InputOption::VALUE_OPTIONAL, 'Duration of each frame in the gif', 20);
+        $this->addOption('loopings', null, InputOption::VALUE_OPTIONAL, 'Number of times to loop the gif before stopping', 0);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -49,19 +52,19 @@ class GenerateGifCommand extends Command
 
         $output->writeln("<comment>Filtering images for the specified timezone...</comment>");
         
-        $data = $epic->filterDataByTimezone($data, $timezone, $input->getOption('margin'));
+        $data = $epic->filterDataByTimezone($data, $timezone, $input->getOption('timezone-margin'));
         $dataCount = count($data);
 
         $output->writeln("<info>Got $dataCount elements.</info>");
         
         $frames = []; $durations = [];
         foreach ($data as $key => $value) {
-            $frames[] = $epic->getImageFromData($value);
-            $durations[] = 20;
+            $frames[] = $epic->getImageFromData($value, $input->getOption('sourcetype'));
+            $durations[] = $input->getOption('frame-duration');
         }
 
         $output->writeln("<comment>Generating final image...<comment>");
-        $generator->create($frames, $durations);
+        $generator->create($frames, $durations, $input->getOption('loopings'));
         file_put_contents('cgi/' . $filename, $generator->getGif());
 
         $output->writeln("<info>Generated $filename with $dataCount frames.</info>");
